4e5cd7f647ba6a3e7270b3dffbaadf757ea546d9
hwpeng <hwpeng20@gmail.com>
2018-03-31 20:00:13 -0700

    revise the train function

--- python/conv.py
+++ python/conv.py
@@ -9,6 +9,7 @@ def conv(inputs, weights):
 
 	L = i_size[0] # inputs length
 	D = i_size[2] # inputs/weights depth
+
 	W = w_size[1] # weights length
 	K = w_size[0] # number of weighs
 
@@ -17,13 +18,13 @@ def conv(inputs, weights):
 
 	outputs = np.zeros((O_L, O_L, O_D))
 
-	for n in xrange(0,K):
-		for y_base in xrange(0,L-W+1):
-			for x_base in xrange(0,L-W+1):
-				for z in xrange(0,D):
-					for y in xrange(y_base,y_base+W):
-						for x in xrange(x_base,x_base+W):
-							outputs[x_base,y_base,n] += inputs[x,y,z]*weights[n,x-x_base,y-y_base] 
+	for n in range(0,K):
+		for y_base in range(0,L-W+1):
+			for x_base in range(0,L-W+1):
+				for z in range(0,D):
+					for y in range(y_base,y_base+W):
+						for x in range(x_base,x_base+W):
+							outputs[x_base,y_base,n] += inputs[x,y,z]*weights[n,x-x_base,y-y_base,z] 
    
 	return outputs
 
@@ -35,8 +36,8 @@ def fc(inputs, weights):
 
 	outputs = np.zeros((output_nums,))
 
-	for o in xrange(0,output_nums):
-		for i in xrange(0,inputs_nums):
+	for o in range(0,output_nums):
+		for i in range(0,inputs_nums):
 			outputs[o] += inputs_ar[i]*weights[i,o]
 
 	return outputs
@@ -69,8 +70,8 @@ def data_generate(i_size, w1_size, w2_size, o_size, nums):
 	inputs = list()
 	target = list()
 
-	for n in xrange(0,nums):
-		i1 = np.random.rand(i_size)
+	for n in range(0,nums):
+		i1 = np.random.rand(i_size[0],i_size[1],i_size[2])
 		inputs.append(i1)
 		i2 = conv(i1, w1_target) 
 		i3 = conv(i2, w2_target)
@@ -79,6 +80,7 @@ def data_generate(i_size, w1_size, w2_size, o_size, nums):
 		pass
 
 	return inputs, target
+
 
 def train(inputs, target, i_size, w1_size, w2_size):
 
@@ -100,19 +102,28 @@ def train(inputs, target, i_size, w1_size, w2_size):
 	w3 = np.random.rand(i3_nums, 1)
 
 	# weights gradients
-	w1_gd = np.zeros(K1, W1, W1, D1)
-	w2_gd = np.zeros(K2, W2, W2, D2)
-	w3_gd = np.zeros(i3_nums, 1)
+	w1_gd = np.zeros((K1, W1, W1, D1))
+	w2_gd = np.zeros((K2, W2, W2, D2))
+	w3_gd = np.zeros((i3_nums, 1))
 
 	# inputs gradients
-	x3_gd = np.zeros(i3_nums)
-	x2_gd = np.zeros((L2,L2,D2))
+	i3_gd = np.zeros((i3_nums,))
+	i2_gd = np.zeros((L2,L2,D2))
 
 	lr = 0.001 # learning rate
 
 	data_nums = len(target) # dataset size
 
-	for n in xrange(0,data_nums):
+	bf_train = list()
+	for n in range(0,data_nums):
+		i2 = conv(inputs[n], w1) 
+		i3 = conv(i2, w2)
+		o = fc(i3, w3)
+		bf_train.append(o)
+
+
+
+	for n in range(0,data_nums):
 
 		# feed-forward propagation
 		i1 = inputs[n]
@@ -123,24 +134,51 @@ def train(inputs, target, i_size, w1_size, w2_size):
 
 		# backward propagation
 		t = target[n][0]
-		e_gd = o-t # deravetive of 0.5*(o-t)**2
+		o_gd = o-t # deravetive of 0.5*(o-t)**2
 
 		# fc gradients
-		w3_gd = conv(i3, e_gd)
-		x3_gd = conv(w3, e_gd)
+		# o_gd = np.reshape(o_gd, (1,1,1,1))
+		w3_gd = i3*o_gd
+		i3_gd = w3*o_gd
 
-		w3_gd = w3_gd.flatten()
+		w3_gd = np.reshape(w3_gd, (i3_nums, 1))
 		w3 -= lr*w3_gd
 
 		# conv2 gradients
-		w2_gd = conv(i2, x3_gd)
-		
+		i3_gd_rs = np.reshape(i3_gd, (K2, (L2-W2+1), (L2-W2+1), 1))
+		for d in range(0,D2):
+			tmp = conv(i2[:,:,d].reshape((L2,L2,1)), i3_gd_rs) #
+			for i in range(0,K2):
+				w2_gd[i,:,:,d] = tmp[:,:,i]
 
 
+		w2_rs = np.zeros((D2, W2, W2, K2))
+		for i in range(0,K2):
+			for j in range(0,D2):
+				w2_rs[j,:,:,i] = w2[i,:,:,j] # w2 reshape
+		w2_rsrt = np.rot90(w2_rs,2) # rotate 180
 
-		pass
+		pad_num = W2-1
+		i3_gd_pad = np.pad(np.reshape(i3_gd, ((L2-W2+1), (L2-W2+1), K2)), ((pad_num,pad_num), (pad_num,pad_num), (0,0)), 'constant')
+		i2_gd = conv(i3_gd_pad, w2_rsrt)
+
+		w2 -= lr*w2_gd
+
+		# conv1 gradients
+		i2_gd = np.reshape(i2_gd, (K1, L2, L2, 1))
+		for d in range(0,D1):
+			tmp = conv(i1[:,:,d].reshape((L1,L1,1)), i2_gd)
+			for i in range(0,K1):
+				w1_gd[i,:,:,d] = tmp[:,:,i]
+
+		w1 -= lr*w1_gd
+        
+	af_train = list()
+	for n in range(0,data_nums):
+		i2 = conv(inputs[n], w1) 
+		i3 = conv(i2, w2)
+		o = fc(i3, w3)
+		af_train.append(o)
 
 
-
-	pass
-
+	return bf_train, af_train
