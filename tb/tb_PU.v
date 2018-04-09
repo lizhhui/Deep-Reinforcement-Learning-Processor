7944a9c17d01f407e89284068ef126a5581484ab
hwpeng <hwpeng20@gmail.com>
2018-04-08 21:37:19 -0700

    add tb 2

--- tb/tb_PU.v
+++ tb/tb_PU.v
@@ -1,4 +1,5 @@
-module PU_tb
+// Testbench for PU
+module tb_PU
 #(parameter
 	DATA_WIDTH = 8,
 	NUM_MAC4 = 16,
@@ -8,34 +9,58 @@ module PU_tb
 	RADDR_WIDTH = 6 // depth = 64, 176B
 	)
 (
-	// inputs
-	input clk,
-	input rst_n,
-
-	// to MAC_cluster
-	input [TOTAL_INPUT_WIDTH-1:0] in_data,
-	input in_add_bias, // 1: add bias, 0: no bias
-	input in_relu, // 1: w/ relu, 0: w/o relu
-	input in_done, // the flag for the last partial sum
-	input in_cache_clear,
-	input [4:0] in_cache_rd_addr,
-	input [4:0] in_cache_wr_addr,
-
-	// to wmem
-	input in_w_wr_en,
-	input [WADDR_WIDTH-1:0] in_w_wr_addr,
-	input [TOTAL_INPUT_WIDTH-1:0] in_w_wr_data,  
-	input [WADDR_WIDTH-1:0] in_w_rd_addr,
-	input [2:0] in_bias_addr,
-
-	// to rmem
-	input in_r_wr_en,
-	input [WADDR_WIDTH-1:0] in_r_wr_addr,
-	input in_r_rd_en,   
-	input [WADDR_WIDTH-1:0] in_r_rd_addr,
-
-	// outputs
-	output wire signed [TOTAL_OUTPUT_WIDTH-1:0] out_total_sum,
-	output wire signed [TOTAL_OUTPUT_WIDTH-1:0] out_rmem
 
 	);
+
+reg clk, rst_n;
+reg [TOTAL_INPUT_WIDTH-1:0] in_data;
+reg in_add_bias, in_relu, in_done, in_cache_clear;
+reg [4:0] in_cache_rd_addr, in_cache_wr_addr;
+reg in_w_wr_en;
+reg [WADDR_WIDTH-1:0] in_w_wr_addr;
+reg [TOTAL_INPUT_WIDTH-1:0] in_w_wr_data;  
+reg [WADDR_WIDTH-1:0] in_w_rd_addr;
+reg [2:0] in_bias_addr;
+
+reg in_r_wr_en;
+reg [WADDR_WIDTH-1:0] in_r_wr_addr;
+reg in_r_rd_en;   
+reg [WADDR_WIDTH-1:0] in_r_rd_addr;
+
+wire signed [TOTAL_OUTPUT_WIDTH-1:0] out_total_sum;
+wire signed [TOTAL_OUTPUT_WIDTH-1:0] out_rmem;
+
+
+PU PU_inst(
+	.clk(clk),
+	.rst_n(rst_n),
+
+	// to MAC_cluster
+	.in_data(in_data),
+	.in_add_bias(in_add_bias), // 1: add bias, 0: no bias
+	.in_relu(in_relu), // 1: w/ relu, 0: w/o relu
+	.in_done(in_done), // the flag for the last partial sum
+	.in_cache_clear(in_cache_clear),
+	.in_cache_rd_addr(in_cache_rd_addr),
+	.in_cache_wr_addr(in_cache_wr_addr),
+
+	// to wmem
+	.in_w_wr_en(in_w_wr_en),
+	.in_w_wr_addr(in_w_wr_addr),
+	.in_w_wr_data(in_w_wr_data),  
+	.in_w_rd_addr(in_w_rd_addr),
+	.in_bias_addr(in_bias_addr),
+
+	// to rmem
+	.in_r_wr_en(in_r_wr_en),
+	.in_r_wr_addr(in_r_wr_addr),
+	.in_r_rd_en(in_r_rd_en),   
+	.in_r_rd_addr(in_r_rd_addr),
+
+	// outputs
+	.out_total_sum(out_total_sum),
+	.out_rmem(out_rmem)
+
+	);
+
+endmodule
